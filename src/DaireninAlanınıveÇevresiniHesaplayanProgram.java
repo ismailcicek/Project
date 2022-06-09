9902766201f330357af2f45edf8225eca6f3da0e
Ismail Cicek <kids_cocuk@hotmail.com>
2022-06-09 23:27:59 +0300

    DaireninAlanınıveÇevresiniHesaplayanProgram

--- src/DaireninAlanınıveÇevresiniHesaplayanProgram.java
+++ src/DaireninAlanınıveÇevresiniHesaplayanProgram.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class DaireninAlanınıveÇevresiniHesaplayanProgram {
+import java.util.Scanner;
+
+public class DaireninAlanınıveÇevresiniHesaplayanProgram {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int r,a;
+        double pi=3.14,alan;
+
+        System.out.print("Dairenin yarı çapını giriniz :");
+        r = inp.nextInt();
+
+        System.out.print("Dairenin merkez açısının ölçüsünü giriniz :");
+        a = inp.nextInt();
+
+
+        alan = Math.sqrt(pi * (r * r) * a) / 360;
+
+        System.out.print("Dairenin alanı :" + alan);
+
+
+    }
+
 }
