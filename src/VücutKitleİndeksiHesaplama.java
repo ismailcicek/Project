fdce6622613e671f4ad141ff7293d6f7a9155b4e
Ismail Cicek <kids_cocuk@hotmail.com>
2022-06-11 17:35:29 +0300

    VücutKitleİndeksiHesaplama

--- src/VücutKitleİndeksiHesaplama.java
+++ src/VücutKitleİndeksiHesaplama.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class VücutKitleİndeksiHesaplama {
+import java.util.Scanner;
+
+public class VücutKitleİndeksiHesaplama {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        double kilo,boy,kitle;
+
+        System.out.print("Lütfen kilonuzu giriniz :");
+        kilo = inp.nextDouble();
+
+        System.out.print("Lütfen boyunuzu giriniz :");
+        boy = inp.nextDouble();
+
+        kitle = kilo/(boy*boy);
+
+        System.out.print("Vücüt Kitle İndeksiniz :" + kitle);
+
+
+
+
+
+    }
 }
