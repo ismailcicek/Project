05dc99939cb401eb57c907d4494218d2b61781f1
Ismail Cicek <kids_cocuk@hotmail.com>
2022-06-25 20:17:09 +0300

    SıfırdanGirilenSayıların3ve4eTamBölünenSayılarınOrtalamasınıHesaplayanProgram

--- README.md
+++ README.md
@@ -16,5 +16,6 @@
 * UcakBiletiFiyatıHesaplamaProgramı
 * CinZodyagıHesaplamaProgramı
 * ArtıkYIlHesaplamaProgramı
+* SıfırdanGirilenSayıların3ve4eTamBölünenSayılarınOrtalamasınıHesaplayanProgram
 
 ![Lorem Picsum Gorsel](https://cdn.sanity.io/images/9kdepi1d/production/65c832d202a503b15d99e628f4313782f3ef50db-300x62.png)![Lorem Picsum Gorsel]( https://patika-prod.s3.eu-central-1.amazonaws.com/staticFiles/patikaLogo.png)
---
+++ src/SıfırdanGirilenSayıların3ve4eTamBölünenSayılarınOrtalamasınıHesaplayanProgram.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SıfırdanGirilenSayıların3ve4eTamBölünenSayılarınOrtalamasınıHesaplayanProgram {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+        int k = 0, toplam = 0,sayı = 0;
+        double ortalama;
+
+        System.out.print("Bir Sayı Giriniz :");
+        k = input.nextInt();
+
+
+    for (int i = 0; i <= k; i ++){
+        if (i % 3 == 0 && i % 4 == 0){
+        
+        toplam += i;
+        sayı ++;
+
+        }
+
+    }
+    ortalama = toplam/(sayı -1);
+        System.out.println("0'dan girilen sayının 3'e ve 4'e bölümünün ortalaması :" + ortalama);
+    }
+}
