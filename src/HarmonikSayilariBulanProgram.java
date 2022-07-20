2fda4bd58efbb0b7f8188d20bbb752132d5cfc01
Ismail Cicek <kids_cocuk@hotmail.com>
2022-07-20 16:17:43 +0300

    HarmonikSayilariBulanProgram

--- src/HarmonikSayilariBulanProgram.java
+++ src/HarmonikSayilariBulanProgram.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class HarmonikSayilariBulanProgram {
+import java.util.Scanner;
+public class HarmonikSayilariBulanProgram {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("N sayısını giriniz :");
+        int n = input.nextInt();
+        double result = 0.0;
+        for (double i = 1; i <= n; i++){
+            result += (1/i);
+        }
+        System.out.print("Harmonik Sayı :" +result);
+    }
 }
