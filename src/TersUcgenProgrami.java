9d24a93297b4aa6891705add433a9846652b0373
Ismail Cicek <kids_cocuk@hotmail.com>
2022-07-22 17:11:40 +0300

    TersUcgenProgrami

---
+++ src/TersUcgenProgrami.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TersUcgenProgrami {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        System.out.print("Üçgen boyunu giriniz :");
+        n = input.nextInt();
+        for (int i = 0; i <= n; i++) {
+            for (int k = 0; k <= i - 1; k++) {
+                System.out.print(" ");
+            }
+            for (int t = 1; t <= (2 * n) - (i * 2 + 1); t++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
